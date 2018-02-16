5a0448fd7c62861f14e3e192c73a8d3b031d2757
August3 <9rails@gmail.com>
2018-02-16 20:05:24 +0200

    package.json updated for deploy

--- modules/config.js
+++ modules/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     port: process.env['PORT'] || 3000,
-    token: process.env['EXTERMINATE_BOT_TOKEN'] || "514342456:AAFg9Z0iXcarwTvo1I79oGDouvTNJ4Rb00M"
+    token: process.env['EXTERMINATE_BOT_TOKEN']
 };
